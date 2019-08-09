18e0e5a84f6105fe34421f2b6b923a2585968b30
lucas <lucas_buttow@outlook.com>
2019-08-09 11:52:35 -0300

    teste

---
+++ app/src/main/java/br/edu/ifsul/myapp/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package br.edu.ifsul.myapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.edu.ifsul.myapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
